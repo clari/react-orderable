59705bbefd64ef2bdcc2d153a686dddceca0fb23
Matthew King <mking-clari@users.noreply.github.com>
2016-04-06 18:37:17 -0700

    add gh ribbon

--- app/ListContainer.js
+++ app/ListContainer.js
@@ -84,6 +84,19 @@ class ListContainer extends React.Component {
           logger={this.log}
           onChange={this.handleHorizontalChange}
         />
+        {process.env.NODE_ENV === 'production' && <a href="https://github.com/clariussystems/react-orderable">
+          <img
+            alt="Fork me on GitHub"
+            data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"
+            src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67"
+            style={{
+              border: 0,
+              position: 'fixed',
+              top: 0,
+              right: 0,
+            }}
+          />
+        </a>}
       </div>
     );
   }
